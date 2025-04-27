efe517bb18f529c8155a41db4a6dbb3c42aca521
Luigi Camilleri <luigi.camilleri.23@um.edu.mt>
2025-04-27 15:45:54 +0200

    Add handle single character tokens

--- lexerassignments.py
+++ lexerassignments.py
@@ -1,13 +1,31 @@
 #Class to wrap the different tokens we'll be using
 from enum import Enum
+
 class TokenType(Enum):
     identifier = 1
     integer = 2
-    whitespace = 3
-    equals = 4
-    semicolon = 5    
-    error = 6
-    end = 7
+    equals = 3
+    semicolon = 4
+    plus = 5
+    minus = 6
+    multiply = 7
+    slash = 8
+    less = 9
+    greater = 10
+    excl = 11
+    lparen = 12
+    rparen = 13
+    lbrace = 14
+    rbrace = 15
+    lbracket = 16
+    rbracket = 17
+    colon = 18
+    comma = 19
+    hash = 20
+    dot = 21
+    error = 22
+    end = 23
+
 
 class Token:
     def __init__(self, t, l):
@@ -17,9 +35,13 @@ class Token:
 #Lexer class wrapper for code above
 class Lexer:
     def __init__(self):
-        self.lexeme_list = ["_", "letter", "digit", "eq", "sc", "other"]
-        self.states_list = [0, 1, 2, 3, 4, 5]
-        self.states_accp = [1, 2, 3, 4, 5]
+        self.lexeme_list = ["letter", "digit", "underscore", "plus", "minus",
+                            "multiply", "slash", "equals", "less", "greater", 
+                            "excl", "lparen", "rparen", "lbrace", "rbrace",
+                            "lbracket", "rbracket", "colon", "comma", "semicolon",
+                            "hash", "dot","other"]
+        self.states_list = list(range(22))
+        self.states_accp = list(range(1, 22))   
 
         self.rows = len(self.states_list)
         self.cols = len(self.lexeme_list)
@@ -29,22 +51,37 @@ class Lexer:
         self.InitialiseTxTable();     
 
     def InitialiseTxTable(self):
-        # Update Tx to represent the state transition function of the DFA
-        # Variables
+        # Identifiers: start with letter or underscore, followed by letters/digits
         self.Tx[0][self.lexeme_list.index("letter")] = 1
-        self.Tx[0][self.lexeme_list.index("_")] = 1
+        self.Tx[0][self.lexeme_list.index("underscore")] = 1
         self.Tx[1][self.lexeme_list.index("letter")] = 1
         self.Tx[1][self.lexeme_list.index("digit")] = 1
+        self.Tx[1][self.lexeme_list.index("underscore")] = 1
 
-        #Eq sign (=)
-        self.Tx[0][self.lexeme_list.index("eq")] = 3        
+        # Integers: sequence of digits
+        self.Tx[0][self.lexeme_list.index("digit")] = 2
+        self.Tx[2][self.lexeme_list.index("digit")] = 2
 
-        #Integers
-        self.Tx[0][self.lexeme_list.index("digit")] = 4        
-        self.Tx[4][self.lexeme_list.index("digit")] = 4        
-
-        #Semicolon sign (;)
-        self.Tx[0][self.lexeme_list.index("sc")] = 5
+        # Single-character operators & symbols (direct transitions to unique states)
+        self.Tx[0][self.lexeme_list.index("equals")] = 3
+        self.Tx[0][self.lexeme_list.index("semicolon")] = 4
+        self.Tx[0][self.lexeme_list.index("plus")] = 5
+        self.Tx[0][self.lexeme_list.index("minus")] = 6
+        self.Tx[0][self.lexeme_list.index("multiply")] = 7
+        self.Tx[0][self.lexeme_list.index("slash")] = 8
+        self.Tx[0][self.lexeme_list.index("less")] = 9
+        self.Tx[0][self.lexeme_list.index("greater")] = 10
+        self.Tx[0][self.lexeme_list.index("excl")] = 11
+        self.Tx[0][self.lexeme_list.index("lparen")] = 12
+        self.Tx[0][self.lexeme_list.index("rparen")] = 13
+        self.Tx[0][self.lexeme_list.index("lbrace")] = 14
+        self.Tx[0][self.lexeme_list.index("rbrace")] = 15
+        self.Tx[0][self.lexeme_list.index("lbracket")] = 16
+        self.Tx[0][self.lexeme_list.index("rbracket")] = 17
+        self.Tx[0][self.lexeme_list.index("colon")] = 18
+        self.Tx[0][self.lexeme_list.index("comma")] = 19
+        self.Tx[0][self.lexeme_list.index("hash")] = 20
+        self.Tx[0][self.lexeme_list.index("dot")] = 21
 
         for row in self.Tx:
             print(row)
@@ -60,25 +97,102 @@ class Lexer:
         if state == 1:
             return Token(TokenType.identifier, lexeme)
         elif state == 2:
-            return Token(TokenType.whitespace, lexeme)
+            return Token(TokenType.integer, lexeme)
         elif state == 3:
             return Token(TokenType.equals, lexeme)
         elif state == 4:
-            return Token(TokenType.integer, lexeme)
-        elif state == 5:
             return Token(TokenType.semicolon, lexeme)
+        elif state == 5:
+            return Token(TokenType.plus, lexeme)
+        elif state == 6:
+            return Token(TokenType.minus, lexeme)
+        elif state == 7:
+            return Token(TokenType.multiply, lexeme)
+        elif state == 8:
+            return Token(TokenType.slash, lexeme)
+        elif state == 9:
+            return Token(TokenType.less, lexeme)
+        elif state == 10:
+            return Token(TokenType.greater, lexeme)
+        elif state == 11:
+            return Token(TokenType.excl, lexeme)
+        elif state == 12:
+            return Token(TokenType.lparen, lexeme)
+        elif state == 13:
+            return Token(TokenType.rparen, lexeme)
+        elif state == 14:
+            return Token(TokenType.lbrace, lexeme)
+        elif state == 15:
+            return Token(TokenType.rbrace, lexeme)
+        elif state == 16:
+            return Token(TokenType.lbracket, lexeme)
+        elif state == 17:
+            return Token(TokenType.rbracket, lexeme)
+        elif state == 18:
+            return Token(TokenType.colon, lexeme)
+        elif state == 19:
+            return Token(TokenType.comma, lexeme)
+        elif state == 20:
+            return Token(TokenType.hash, lexeme)
+        elif state == 21:
+            return Token(TokenType.dot, lexeme)
         else:
-            return 'default result'
+            return Token(TokenType.error, lexeme)
+
 
 
     def CatChar(self, character):
-        cat = "other"
-        if character.isalpha(): cat = "letter"
-        if character.isdigit(): cat = "digit"
-        if character == "_": cat = "_"
-        if character == ";": cat = "sc"
-        if character == "=": cat = "eq"
-        return cat
+
+        if character.isalpha():
+            return "letter"
+        elif character.isdigit():
+            return "digit"
+        elif character == "_":
+            return "underscore" # underscore is treated like an identifier not a symbol in PArl
+
+        # Operators
+        elif character == "+":
+            return "plus"
+        elif character == "-":
+            return "minus"
+        elif character == "*":
+            return "multiply"
+        elif character == "/":
+            return "slash"        
+        elif character == "=":
+            return "equals"
+        elif character == "<":
+            return "less"
+        elif character == ">":
+            return "greater"
+        elif character == "!":
+            return "excl"
+
+        # Symbols
+        elif character == "(":
+            return "lparen"
+        elif character == ")":
+            return "rparen"
+        elif character == "{":
+            return "lbrace"
+        elif character == "}":
+            return "rbrace"
+        elif character == "[":
+            return "lbracket"
+        elif character == "]":
+            return "rbracket"
+        elif character == ":":
+            return "colon"
+        elif character == ",":
+            return "comma"
+        elif character == ";":
+            return "semicolon"
+        elif character == "#":
+            return "hash"
+        elif  character == ".":
+            return "dot"
+        else:
+            return "other"
 
     def EndOfInput(self, src_program_str, src_program_idx):
         if (src_program_idx > len(src_program_str)-1):
@@ -180,7 +294,7 @@ class Lexer:
         return tokens_list;
 
 lex = Lexer()
-toks = lex.GenerateTokens("x=23;y=100;   z=;")
+toks = lex.GenerateTokens("x=5+3-2*4/2<10>1!=0;(arr[2]){key:value,next:item#123.456;}")
 
 for t in toks:
     print(t.type, t.lexeme)
